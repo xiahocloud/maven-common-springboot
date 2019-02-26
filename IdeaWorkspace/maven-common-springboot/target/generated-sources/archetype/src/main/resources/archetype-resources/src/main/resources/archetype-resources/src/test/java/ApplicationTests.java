5d6f2a82749755a0ccd0336bb91dbef0e541dfd4
andy <wanghaoxin5277@gmail.com>
2019-02-26 23:46:01 +0800

    初始化maven模板

---
+++ IdeaWorkspace/maven-common-springboot/target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/resources/archetype-resources/src/test/java/ApplicationTests.java
@@ -0,0 +1,17 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${symbol_dollar}{package};
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
