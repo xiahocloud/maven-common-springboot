0177e692a1aa4bbb98964a26992fa755c30ccf86
andy <wanghaoxin5277@gmail.com>
2019-04-03 09:43:31 +0800

    import package

--- src/main/resources/archetype-resources/src/main/java/config/MasterDataSourceConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/MasterDataSourceConfig.java
@@ -1,17 +1,21 @@
 package ${package}.config;
-
-		import com.alibaba.druid.pool.DruidDataSource;
-		import org.apache.ibatis.session.SqlSessionFactory;
-		import org.mybatis.spring.SqlSessionFactoryBean;
-		import org.mybatis.spring.SqlSessionTemplate;
-		import org.springframework.beans.factory.annotation.Value;
-		import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-		import org.springframework.context.annotation.Bean;
-		import org.springframework.context.annotation.Configuration;
-		import org.springframework.context.annotation.Primary;
-		import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-		import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-		import org.springframework.transaction.annotation.EnableTransactionManagement;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Andy
---
+++ target/classes/archetype-resources/src/main/resources/template/freemarker/modules/demo/demo.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Demo(示例)</title>
+</head>
+<body>
+This Is Demo Page（这里是示例页面）
+</body>
+</html>
