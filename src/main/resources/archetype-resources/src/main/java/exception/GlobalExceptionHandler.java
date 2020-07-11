60e4abab16923fdb1d666668c1b9433ef56c5c36
andy <wanghaoxin5277@gmail.com>
2020-07-11 18:37:03 +0800

    修复全局异常处理

--- src/main/resources/archetype-resources/src/main/java/exception/GlobalExceptionHandler.java
+++ src/main/resources/archetype-resources/src/main/java/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package ${package}.exception;
 
 import com.alibaba.fastjson.JSON;
-import com.hnps.stockfamily.common.HttpStatusEnum;
-import com.hnps.stockfamily.common.ViewResult;
+import ${package}.common.HttpStatusEnum;
+import ${package}.common.ViewResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
